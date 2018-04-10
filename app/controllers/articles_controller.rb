a6d4803c72fa0713e45978412b96594ae3d5f274
Germain <germain.prevot@gmail.com>
2018-04-10 17:30:14 +0200

    admin finit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+
 
   # GET /articles
   # GET /articles.json
