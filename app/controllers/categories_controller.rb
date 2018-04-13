59a32c6abc58de366d7e43b00a195af99d46ea4e
Nicolas Guitard <ngui@caramail.fr>
2018-04-13 13:49:31 +0200

    category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,6 +12,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @articles = @category.articles
   end
 
   # GET /categories/new
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :categories
   devise_for :users
   resources :users
-
+  resources :categories
   resources :articles
-  root to: "articles#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "articles#index"
 end
